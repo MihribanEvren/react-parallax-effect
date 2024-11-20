c9dc092d283afc3b024044a410497ea428cbe960
Mihriban Evren <mihribanevrenn@gmail.com>
2024-11-20 22:38:49 +0300

    tailwind and motion installed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col gap-10 overflow-x-hidden">
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
